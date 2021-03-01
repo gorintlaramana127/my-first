d7ac5cebfc079711d4a2fde8af0513829e5c28a6
ramana <gorintlaramana001@gmail.com>
2021-03-01 13:22:00 +0530

    i'm uploading my first project

---
+++ src/com/company/Dog.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Dog {
+
+
+
+        String name = "tommy";
+        int cost = 20;
+        String color = "white";
+    }
+
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+
+        public static void main (String[]args){
+
+
+                // write your code here
+            Dog d = new Dog();
+            System.out.println(d.name);
+
+        }
+    }
+}
